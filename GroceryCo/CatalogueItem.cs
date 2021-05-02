0c9def3183cfc146eb5c3a85d12ad4de4a470d54
joe-bun <joe-bun@users.noreply.github.com>
2021-05-02 00:00:29 -0600

    Initial commit

---
+++ GroceryCo/CatalogueItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryCo
+{
+    class CatalogueItem
+    {
+        public string name;
+        public decimal price;
+        public bool onSale;
+        public decimal onSalePrice;
+        public int quantity;
+
+        public CatalogueItem(string name, string price)
+        {
+            this.name = name;
+            this.price = Convert.ToDecimal(price);        
+        }
+
+        public decimal setOnSalePrice(string promotionalPrice) => this.onSalePrice = Convert.ToDecimal(promotionalPrice);
+        public decimal getEffectivePrice()
+        {
+                return onSale ? onSalePrice : price;
+        }
+        public string getItem() => name;
+        public void addItemToReceipt(string name)
+        {
+
+        }
+    }
+}
